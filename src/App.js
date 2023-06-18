b96396a4dd04b47455badcdbb22b00f919d81f6f
nGhart <naaghart@gmail.com>
2023-06-18 17:48:43 +0000

    gitcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import AddExpense from './Components/AddExpense';
+import ExpensesList from './Components/ExpensesList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1
+        style={{
+          textAlign: 'center',
+          fontFamily: 'Gasoek One',
+          margin: '15px',
+        }}
+      >
+        Expense Tracker
+      </h1>
+      <Container>
+        <Row>
+          <Col xs={{ span: 10, offset: 1 }} sm={{ span: 4, offset: 4 }}>
+            <AddExpense />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={{ span: 8, offset: 2 }}>
+            <ExpensesList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
