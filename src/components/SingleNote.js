2bd6fe15274d9ae6e391ae92d203c301b9f07d0e
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-21 12:30:38 +0000

    text formatting

--- src/components/SingleNote.js
+++ src/components/SingleNote.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import EditNote from './EditNote';
 import { useDispatch } from 'react-redux';
 import { deleteNote } from '../slice/appSlice';
 
 function SingleNote(props) {
   const dispatch = useDispatch();
+  const [bold, setBold] = useState(true);
+  const handleBold = () => {
+    setBold(!bold);
+  };
+  const [underline, setUnderline] = useState(true);
+  const handleUnderline = () => {
+    setUnderline(!underline);
+  };
+  const [italic, setItalic] = useState(true);
+  const handleItalic = () => {
+    setItalic(!italic);
+  };
+  const [strike, setStrike] = useState(true);
+  const handleStrike = () => {
+    setStrike(!strike);
+  };
 
   return (
     <>
@@ -13,7 +29,6 @@ function SingleNote(props) {
           <h6>{props.user.title}</h6>
 
           <span className="icons">
-            {/* <EditNote user={props.user} editNote={props.editNote} /> */}
             <span
               className="material-symbols-outlined"
               style={{
@@ -29,15 +44,60 @@ function SingleNote(props) {
             </span>
           </span>
         </div>
-        <div className="postitBody" style={{ textDecoration: 'underline' }}>
+        <div className="postitBody" style={{}}>
           <p>{props.user.date}</p>
-          {props.user.notetext}
+          <span
+            style={{
+              fontWeight: bold ? 'normal' : 700,
+              textDecoration: underline ? 'none' : 'underline',
+              fontStyle: italic ? 'normal' : 'italic',
+              textDecorationLine: strike ? 'none' : 'line-through',
+            }}
+          >
+            {props.user.notetext}
+          </span>
         </div>
         <div className="postitFooter">
-          <p style={{ fontWeight: 700 }}>B</p>
-          <p style={{ textDecoration: 'underline' }}>U</p>
-          <p style={{ fontStyle: 'italic' }}>I</p>
-          <p style={{ textDecoration: 'line-through' }}>S</p>
+          <button
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              fontWeight: 700,
+            }}
+            onClick={handleBold}
+          >
+            <p>B</p>
+          </button>
+          <button
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              textDecoration: 'underline',
+            }}
+            onClick={handleUnderline}
+          >
+            U
+          </button>
+          <button
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              fontStyle: 'italic',
+            }}
+            onClick={handleItalic}
+          >
+            I
+          </button>
+          <button
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              textDecoration: 'line-through',
+            }}
+            onClick={handleStrike}
+          >
+            S
+          </button>
         </div>
       </div>
     </>
